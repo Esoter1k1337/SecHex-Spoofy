bd072981211d40320378bbf132ead8d2fcbf6d99
Yaroslav Efimov <146434433+Flywey1337@users.noreply.github.com>
2025-04-22 21:14:59 +0900

    Update Program.cs
    h hghggfhd

--- SecHex-GUI/SecHex-GUI/Program.cs
+++ SecHex-GUI/SecHex-GUI/Program.cs
@@ -1,13 +1,2 @@
 namespace SecHex_GUI
 {
-    internal static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Spoofy());
-            Application.EnableVisualStyles();
-        }
-    }
-}
